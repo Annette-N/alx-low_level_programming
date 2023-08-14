f50bb08e99c64da9ab542c38aef3517cd6c62470
Annette-N <muigaiannette@gmail.com>
2023-08-14 15:13:54 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ * program that prints- and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * return: 1 -(failure)
+ */
+int main(void)
+{
+	fwrite(and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
