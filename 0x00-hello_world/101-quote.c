c041b15dee610561ca5c2e986de8585bc865dcd5
Annette-N <muigaiannette@gmail.com>
2023-08-14 16:04:00 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main-entry point
- * program that prints- and that piece of art is useful" - Dora Korpar, 2015-10-19
- * return: 1 -(failure)
+ * main - A c program that prints a line to the standard error
+ * return: 1 -(success)
  */
 int main(void)
 {
-	fwrite(and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
