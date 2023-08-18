c5786a4260ed0787c34c41bd1bcfa0215eb6a2aa
Annette-N <muigaiannette@gmail.com>
2023-08-18 16:19:39 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,26 +4,34 @@
 *main - fund prints the numbers 1-100, but for multiples of three and five
 *Fizz is printed instead of 3 and is multiples
 *Buzz is printed instead of 5  and its multiples
-*FizzBuzz is printed instead of the muliples of 3 and 5
+*FizzBuzz is printed instead of the muliples of both 3 and 5
 *Return: Always 0
 */
 int main(void)
 {
 int num;
+
 for (num = 1; num <= 100; num++)
 {
-if ((num & 3) == 0 && (num %5) == 0)
+
+if ((num & 3) == 0 && (num % 5) == 0)
 printf("FizzBuzz");
+
 else if ((num % 3) == 0)
 printf("Fizz");
-else if ((num %5) ==0)
+
+else if ((num % 5) ==0)
 printf("Buzz");
+
 else
 printf("%d",num);
+
 if (num == 100)
 continue;
 printf(" ");
 }
+
 printf("\n");
+
 return (0);
 }
