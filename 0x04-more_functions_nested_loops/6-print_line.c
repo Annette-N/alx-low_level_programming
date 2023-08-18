3941ad3ca6a4184ff921e087c5069a14783f1409
Annette-N <muigaiannette@gmail.com>
2023-08-18 14:47:49 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - fund draws a straight line in the terminal
+ * @n: number of lines the character _ to be printed
+ */
+void print_line(int n)
+{
+if (n <= 0)
+{
+putchar('\n');
+}
+else
+{
+int i;
+for (i = 1; i<=n; i++)
+{
+putchar('_');
+}
+putchar('\n');
+}
+}
