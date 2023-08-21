b957458abf636b9a9eb0b8f2e036e548edbf3c81
Annette-N <muigaiannette@gmail.com>
2023-08-21 09:11:55 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that takes a pointer to an int
+ * @n: Declare the value of n
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+int n = 98;
+int *ip;
+ip = &var;
+putchar("Address of var variable: %x\n", &var  );
+return (0);
+}
