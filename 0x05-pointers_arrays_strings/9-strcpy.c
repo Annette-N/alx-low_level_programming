efcb3befa413e6aaf279ce40581b60fb96430fe0
Annette-N <muigaiannette@gmail.com>
2023-08-22 19:27:16 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,14 +4,17 @@
  * *_strcpy - copies the string pointed to by src
  * @dest: char type string
  * @src: char type string
+ * Description: copy string from 'src' to 'dest'
  * Return: pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
 int z = -1;
-do{
+do
+{
 z++;
 dest[z] = src[z];
-} while (src[z] != '\0');
+}
+while (src[z] != '\0');
 return (dest);
 }
