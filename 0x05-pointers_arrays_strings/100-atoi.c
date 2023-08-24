d4e29a3cad970a851ae6005ef9ab172ee1a09ae0
Annette-N <muigaiannette@gmail.com>
2023-08-24 10:31:27 -0700

    0-strcat.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _atoi - function that converts string to integer
+ * @str: string to be converted
+ */
+int _atoi(char *s) {
+    int result = 0;
+    int sign = 1;
+    int i = 0;
+     if (s[i] == '-') {
+        sign = -1;
+        i++;
+    } else if (s[i] == '+') {
+        i++;
+    }
+     while (s[i] >= '0' && s[i] <= '9') {
+	     if (result > (INT_MAX - (s[i] - '0')) / 10) {
+		      return (sign == 1) ? INT_MAX : INT_MIN;
+        }
+        result = result * 10 + (s[i] - '0');
+        i++;
+    }
+
+    return result * sign;
+}
+
