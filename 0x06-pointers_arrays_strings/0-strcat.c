37d8378e0689d0e45655782d2921c408cdf241f3
Annette-N <muigaiannette@gmail.com>
2023-08-24 10:36:04 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,20 +11,20 @@
 
 char *_strcat(char *dest, char *src)
 {
-        int i, j, n, k;
+int i, j, n, k;
 
-        for (i = 0 ; dest[i] != '\0' ; i++)
-                ;
-        for (j = 0 ; src[j] != '\0'; j++)
-                ;
-        n = i + j;
-        j = 0;
-        dest[i - 1] = ' ';
+for (i = 0 ; dest[i] != '\0' ; i++)
+;
+for (j = 0 ; src[j] != '\0'; j++)
+;
+n = i + j;
+j = 0;
+dest[i - 1] = ' ';
 
-        for (k = i ; k < n ; k++)
-        {
-                dest[k] = src[j];
-                j++;
-        }
-        return (dest);
+for (k = i ; k < n ; k++)
+{
+dest[k] = src[j];
+j++;
+}
+return (dest);
 }
